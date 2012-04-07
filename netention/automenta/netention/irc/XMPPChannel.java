913fdb909e21f5de4ac78d2cacc535ea3a3ba1ee
SeH <>
2012-04-07 14:53:27 -0400

    added Smack XMPP library for XMPPChannel

---
+++ netention/automenta/netention/irc/XMPPChannel.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package automenta.netention.irc;
+
+/**
+ *
+ * @author SeH
+ */
+public class XMPPChannel {
+    //http://abhijeetmaharana.com/blog/2007/10/28/writing-a-gtalk-jabberxmpp-client/
+    
+}
